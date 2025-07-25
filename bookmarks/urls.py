4d3541c15093fd96642cbf1a1eb95e31a092d332
I_LU <dhwmzheld6470@gmail.com>
2025-07-25 12:59:40 +0900

    Delete bookmarks directory

--- bookmarks/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.bookmark_list, name='bookmark_list'),
-]
--- bookmarks/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Bookmark
-
-def bookmark_list(request):
-    bookmarks = Bookmark.objects.all()
-    context = {'bookmarks': bookmarks}
-    return render(request, 'bookmarks/bookmark_list.html', context)
